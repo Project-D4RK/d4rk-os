aa218947b2704a68d1192100cceb5e32b24a87f0
AJHblu <hennessan@students.dsbn.org>
2020-10-10 18:08:17 -0400

    remove kde customization

--- config/includes.chroot/usr/share/d4rk/bash/tool-installs/0700-fsociety-install.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-
-set -e
-
-git clone https://github.com/Manisso/fsociety.git; cd fsociety; chmod +x install.sh; ./install.sh; cd ..; rm -rf fsociety
--- config/includes.chroot/usr/share/d4rk/bash/tool-installs/0701-install-john.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-
-set -e
-
-wget "http://http.kali.org/pool/main/j/john/john_1.9.0-Jumbo-1-0kali3_amd64.deb"; wget "http://http.kali.org/pool/main/j/john/john-data_1.9.0-Jumbo-1-0kali3_all.deb"; wget "http://http.kali.org/pool/main/j/john/john-dbgsym_1.9.0-Jumbo-1-0kali3_amd64.deb"; dpkg -i john-data_1.9.0-Jumbo-1-0kali3_all.deb; dpkg -i john_1.9.0-Jumbo-1-0kali3_amd64.deb;  dpkg -i john-dbgsym_1.9.0-Jumbo-1-0kali3_amd64.deb; rm -rf john-dbgsym_1.9.0-Jumbo-1-0kali3_amd64.deb; rm -rf john-data_1.9.0-Jumbo-1-0kali3_all.deb; rm -rf john_1.9.0-Jumbo-1-0kali3_amd64.deb
-
